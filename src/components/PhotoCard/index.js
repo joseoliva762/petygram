183dcd8f6c78ff2baeed21a2d7fb192231b2ca49
jose oliva <itslivcode@gmail.com>
2022-05-03 18:36:17 -0500

    [ADD] toggleLike Hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNearScreen } from '@hooks/useNearScreen';
 import { useLocalStorage } from '@hooks/useLocalStorage';
+import { useToggleLike } from '@hooks/useToggleLike';
 import { CardWrapper, Anchor, ImageWrapper, Image } from '@components/PhotoCard/styles';
 import { FavButton } from '@components/FavButton';
 const DEFAULT_IMAGE = 'https://source.unsplash.com/random';
@@ -9,8 +10,15 @@ export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
   const { show, elementRef } = useNearScreen();
   const key = `like-${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
+  const { mutation } = useToggleLike();
 
   const handleLike = (value) => {
+    value &&
+      mutation({
+        variables: {
+          input: { id }
+        }
+      });
     setLiked(value);
   };
 
